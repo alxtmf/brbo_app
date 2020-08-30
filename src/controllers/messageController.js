e05f3a7bdace93014185f3a82f29acb9515318c9
Bulat <zbd@sibdigital.net>
2020-08-30 21:51:38 +0800

    task-658 3h доработал ql-mutation запрос вставка нового сообщения

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,18 +1,53 @@
+const uuid = require('uuid');
 const {logger} = require("../log");
 const {GraphQLClient, gql} = require('graphql-request')
 const endpoint = `http://localhost:${process.env.PORT}/graphql`
-
-module.exports.sendMessage = function(request, response){
-    if(!request.body) return response.sendStatus(400);
-
-    const graphQLClient = new GraphQLClient(endpoint )//, {
+const graphQLClient = new GraphQLClient(endpoint )//, {
 //     headers: {
 //         authorization: 'Bearer MY_TOKEN',
 //     },
 // })
 
+/*
+test query in Postman
+curl --location --request POST 'localhost:3000/message/send' \
+--header 'Content-Type: application/json' \
+--data-raw '{ "messages": [
+    { "event_type": "EVENT_1", "user_id": "login1", "text": "message_text"}
+]}'
+ */
+
+async function saveMessage(message){
+    try {
+        console.log(`message:` + message);
+
+        const data = await graphQLClient.request(gql`
+                    mutation {
+                        __typename
+                        createRegSentMessage(input: {regSentMessage: {
+                            idEventType: "${message.idEventType}", 
+                            idTargetSystem: "${message.idTargetSystem}", 
+                            uuid: "${uuid.v4()}", 
+                            text: "${message.text}", 
+                            idUser: "${message.idUser}", 
+                            status: 0, 
+                            dateCreate: "${new Date().toISOString()}"}}) {
+                            clientMutationId
+                        }
+                    }
+
+            `
+            )
+        return Promise.resolve({ error: '', data: data})
+    } catch(e){
+        return Promise.reject({ error: e, data: null })
+    }
+}
+
+module.exports.sendMessage = function(request, response){
+    if(!request.body) return response.sendStatus(400);
+
     const messages = request.body.messages
-    let results = []
 
     const promises = messages.map(async (message) => {
         try {
@@ -22,6 +57,7 @@ module.exports.sendMessage = function(request, response){
                 {
                     allClsEventTypes(condition: {code: "${message.event_type}", isDeleted: false}) {
                         nodes {
+                            uuid
                             idTargetSystem
                             clsTargetSystemByIdTargetSystem {
                                 regTargetSystemUsersByIdTargetSystem(condition: {outerId: "${message.user_id}", isDeleted: false}) {
@@ -37,7 +73,6 @@ module.exports.sendMessage = function(request, response){
                 }
             `)
             console.log("result: " + JSON.stringify(data));
-
 /*
 {
   "data": {
@@ -64,13 +99,21 @@ module.exports.sendMessage = function(request, response){
 }
 */
 
-            if(data.allClsEventTypes.nodes.uuid == "" && data.allClsEventTypes.nodes.clsTargetSystemByIdTargetSystem.regTargetSystemUsersByIdTargetSystem.edges.length == 0){
-                //results.push({message: message, result: "event_type not found"})
+            if(data.allClsEventTypes.nodes[0].uuid == "" && data.allClsEventTypes.nodes[0].clsTargetSystemByIdTargetSystem.regTargetSystemUsersByIdTargetSystem.edges.length == 0){
                 return {message: message, result: "event_type not found"}
             } else{
-                //results.push({message: message, result: "ok"})
-                // graphql mutation
-                return {message: message, result: "ok"}
+                message.idEventType = data.allClsEventTypes.nodes[0].uuid
+                message.idTargetSystem = data.allClsEventTypes.nodes[0].idTargetSystem
+                message.idUser = data.allClsEventTypes.nodes[0].clsTargetSystemByIdTargetSystem.regTargetSystemUsersByIdTargetSystem.edges[0].node.idUser
+                console.log(message)
+
+                await saveMessage(message)
+                    .then((result)=> {
+                        return {message: message, result: "save message ok"}
+                    })
+                    .catch((result)=> {
+                        return {message: message, result: result.error}
+                    })
             }
         }catch (e) {
             console.log(JSON.stringify(e))
