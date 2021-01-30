2086f0cba6877f798321078b16ebb4856e56af06
Bulat <zbd@sibdigital.net>
2021-01-30 23:48:49 +0800

    task-661 поправил отправку в viber

--- src/brbo_bottender/index.js
+++ src/brbo_bottender/index.js
@@ -81,13 +81,6 @@ module.exports.taskSentMessages = cron.schedule('*/30 * * * * *', function () {
     timezone: 'Asia/Irkutsk'
 })
 
-// module.exports = async function App(context) {
-//     if (context.event.isText) {
-//         await context.sendText(`recieved the text message: ${context.event.text}`);
-//     }
-// };
-
-
 function generateInlineKeyboard(table) {
     return {
         inlineKeyboard: table.map(row =>
@@ -110,62 +103,10 @@ function generateEventTypeKeyboard(table) {
     };
 }
 
-const mainMenu = {
-    text: 'This is main menu, please click an option.',
-    replyMarkup: generateInlineKeyboard([
-        ['A', 'B'],
-        ['C', 'D'],
-    ]),
-};
-
-const submenuA = {
-    text: 'This is submenu A.',
-    replyMarkup: generateInlineKeyboard([
-        ['A1', 'A2'],
-        ['A3', 'A4'],
-        ['< back to main menu'],
-    ]),
-};
-
-const submenuB = {
-    text: 'This is submenu B.',
-    replyMarkup: generateInlineKeyboard([
-        ['B1', 'B2'],
-        ['B3', 'B4'],
-        ['< back to main menu'],
-    ]),
-};
-
-const submenuC = {
-    text: 'This is submenu C.',
-    replyMarkup: generateInlineKeyboard([
-        ['C1', 'C2'],
-        ['C3', 'C4'],
-        ['< back to main menu'],
-    ]),
-};
-
-const submenuD = {
-    text: 'This is submenu D.',
-    replyMarkup: generateInlineKeyboard([
-        ['D1', 'D2'],
-        ['D3', 'D4'],
-        ['< back to main menu'],
-    ]),
-};
-
-const menuMapping = {
-    '< back to main menu': mainMenu,
-    A: submenuA,
-    B: submenuB,
-    C: submenuC,
-    D: submenuD,
-};
-
-
-
 async function DefaultAction(context) {
-    await context.sendText('Please type "/start" to show the keyboard.');
+    if(context.event.isMessage) {
+        await context.sendText('Please type "/start" to show the keyboard.');
+    }
 }
 
 function buildTelegramKeyboard(keysArr){
@@ -256,16 +197,6 @@ async function AnswerKeyboard(context) {
         .catch(async (err)=> {
             await context.sendText('You are not authorized')
         })
-/*
-    const menu = menuMapping[data];
-    if (menu) {
-        await context.editMessageText(messageId, menu.text, {
-            replyMarkup: menu.menu,
-        });
-    } else {
-        await context.editMessageText(messageId, `Your final choice is ${data}.`);
-    }
-*/
 }
 
 async function TelegramActions(context){
